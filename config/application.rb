60af411ca211ba9f31df66b6d4660bc460e0c210
ayslanhugo <ayslanhugo1@gmail.com>
2025-06-26 09:45:05 -0300

    refactor: Remove configurações de conexão desnecessárias e organize o arquivo database.yml para Rails 8

--- config/application.rb
+++ config/application.rb
@@ -8,14 +8,6 @@ Bundler.require(*Rails.groups)
 module Biblia
   class Application < Rails::Application
     config.load_defaults 8.0
-
-    # Força os componentes do Rails 8 a usarem a base de dados primária.
-    config.active_record.writing_role = :primary
-    config.active_record.reading_role = :primary
-    config.solid_cache.connects_to = { database: { writing: :primary, reading: :primary } }
-    config.solid_queue.connects_to = { database: { writing: :primary, reading: :primary } }
-    config.solid_cable.connects_to = { database: { writing: :primary, reading: :primary } }
-
     config.autoload_lib(ignore: %w[assets tasks])
     config.exceptions_app = self.routes
     config.i18n.default_locale = :'pt-BR'
