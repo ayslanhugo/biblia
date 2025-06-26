576e7ed3192f8006220eddc21ce267865c7848f0
ayslanhugo <ayslanhugo1@gmail.com>
2025-06-26 13:02:47 -0300

    Refactor code structure for improved readability and maintainability

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -1,17 +1,20 @@
 // app/javascript/controllers/datepicker_controller.js
 import { Controller } from "@hotwired/stimulus"
 import flatpickr from "flatpickr"
+import "flatpickr/dist/flatpickr.min.css"
 // Importar a localização em português
 import { Portuguese } from "flatpickr/dist/l10n/pt.js"
 
 export default class extends Controller {
   connect() {
     flatpickr(this.element, {
-      locale: Portuguese, // Usar a tradução
+      locale: Portuguese, // Tradução
       dateFormat: "Y-m-d",
-      onChange: function(selectedDates, dateStr, instance) {
-        // Submete o formulário automaticamente quando uma data é escolhida
-        instance.element.form.requestSubmit();
+      altInput: true,          // Para mostrar um input bonitinho (ex: 26/06/2025)
+      altFormat: "d/m/Y",      // Formato do input alternativo
+      defaultDate: this.element.value || null,  // Já carregar a data selecionada, se houver
+      onChange: () => {
+        this.element.form.requestSubmit();      // Faz o submit automático
       }
     });
   }
