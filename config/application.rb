d0c7b54142c4d5b8a9b82b402bacebd222522a47
ayslanhugo <ayslanhugo1@gmail.com>
2025-06-26 08:44:10 -0300

    Fix: Configura roles de DB para Rails 8

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,14 @@ module Biblia
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
 
+    # Força os componentes do Rails 8 a usarem a base de dados primária.
+    # Isto resolve o erro 'AdapterNotSpecified' em produção.
+    config.active_record.writing_role = :primary
+    config.active_record.reading_role = :primary
+    config.solid_cache.connects_to = { database: { writing: :primary, reading: :primary } }
+    config.solid_queue.connects_to = { database: { writing: :primary, reading: :primary } }
+    config.solid_cable.connects_to = { database: { writing: :primary, reading: :primary } }
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
