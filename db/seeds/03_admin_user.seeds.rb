5a6d80320d09b4ba2629551ce2af2960d9574376
ayslanhugo <ayslanhugo1@gmail.com>
2025-07-02 11:38:46 -0300

    fix: atualiza a lógica de criação do utilizador Admin para garantir a atualização da senha e permissões

--- db/seeds/03_admin_user.seeds.rb
+++ db/seeds/03_admin_user.seeds.rb
@@ -1,23 +1,32 @@
+# db/seeds/03_admin_user.seeds.rb
+
 puts "--------------------------------------------------"
-puts "==> Verificando e criando utilizador Admin..."
+puts "==> Verificando e atualizando utilizador Admin..."
 
 # Defina o e-mail e a senha do seu administrador aqui.
-# IMPORTANTE: Use uma senha forte e segura!
 ADMIN_EMAIL = "admin@master.com"
-ADMIN_PASSWORD = "kJKSZPJ1060@" 
+ADMIN_PASSWORD = "kJKSZPJ1060@" # Use uma senha forte e segura
+# wH9PiNxRPVzFsX7
+# Procura pelo utilizador pelo e-mail.
+# Se não o encontrar, inicializa um novo objeto User.
+admin_user = User.find_or_initialize_by(email: ADMIN_EMAIL)
 
-# Procura pelo utilizador. Se ele não existir, cria-o com a senha definida.
-admin_user = User.find_or_create_by!(email: ADMIN_EMAIL) do |user|
-  user.password = ADMIN_PASSWORD
-  user.password_confirmation = ADMIN_PASSWORD
-  
-  # Se você estiver a usar o módulo :confirmable do Devise, 
-  # descomente a linha abaixo para que não precise de confirmar o e-mail.
-  # user.skip_confirmation! 
-end
+# Define/Atualiza a senha e a confirmação da senha.
+# Isto garante que, mesmo que o utilizador já exista, a sua senha
+# será atualizada para a que está definida aqui.
+admin_user.password = ADMIN_PASSWORD
+admin_user.password_confirmation = ADMIN_PASSWORD
 
-# Garante que o atributo 'admin' do utilizador está definido como verdadeiro.
-admin_user.update!(admin: true)
+# Define a permissão de administrador como verdadeira.
+admin_user.admin = true
 
-puts "==> O utilizador '#{admin_user.email}' agora é um administrador."
+# Se você estiver a usar o módulo :confirmable do Devise, 
+# descomente a linha abaixo para que não precise de confirmar o e-mail.
+# admin_user.skip_confirmation! 
+
+# Salva o utilizador. O 'save!' irá causar um erro se algo falhar,
+# o que é bom para depuração.
+admin_user.save!
+
+puts "==> O utilizador '#{admin_user.email}' agora é um administrador com a senha atualizada."
 puts "--------------------------------------------------"
