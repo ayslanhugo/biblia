2af0d279dcbbb5b708b57ed2dfc5e3e60918d157
ayslanhugo <ayslanhugo1@gmail.com>
2025-07-02 13:00:51 -0300

    feat: aprimora o painel de administração com novas estatísticas e responsividade, atualiza a lógica de edição de utilizadores e melhora a estrutura do layout

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,9 @@
-class Admin::DashboardController < Admin::BaseController
+class Admin::DashboardController < Admin::BaseController # Garanta que herda do seu controller base de admin
   def show
-    # Aqui podemos adicionar estatísticas no futuro
     @user_count = User.count
     @challenge_count = Challenge.count
+    @note_count = Note.count
+    @favorite_count = FavoriteVerse.count
+    @recent_users = User.order(created_at: :desc).limit(5)
   end
 end
